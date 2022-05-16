e12593d3a4bcd6fe14cf9c57ec4d7ebd833b28a0
shehzad <shehzaddiqbal@gmail.com>
2022-05-16 22:15:59 +0500

    js

---
+++ index.js
@@ -0,0 +1,26 @@
+//chapter 2
+//task 1
+var username;
+//task 2
+var myname = "Muhammad Arif";
+//task 3
+var message = "hello " + myname;
+alert(message);
+//task 4
+username = prompt("what is your user name", myname);
+var age = prompt("enter your age");
+var program = prompt("which program you are enrolled in ?");
+alert(username);
+alert(age);
+alert(program);
+//task 5
+alert("Not Secure");
+//task 6
+var email = "123@gmail.com"
+alert("my email address is " + email);
+//task 7
+var book = "A smarter way to learn JavaScript"
+alert("i am reading " + book);
+//task 9
+var pattern = "“This Is My First Assignment”";
+alert(pattern);
